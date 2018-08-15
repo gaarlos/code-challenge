e73371f2e24ea612a63c7da61881e5a9de350641
Garlos <garlos.daw@gmail.com>
2018-08-15 18:01:57 +0200

    Added logo to header

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -1,5 +1,10 @@
 import React from 'react';
 
 export default function() {
-  return <header className="header" />;
+  return (
+    <header className="header">
+      <img src="/assets/images/billin-logo.png" alt="Billin" />
+      <span>Code Challenge</span>
+    </header>
+  );
 }
