154bf451c45aa4f3aa6433b74d0f12c157832677
Garlos <garlos.daw@gmail.com>
2018-08-15 17:46:58 +0200

    Added loading component (WIP)

--- src/layout/Content.jsx
+++ src/layout/Content.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Content = ({ children }) => <div className="content">{children}</div>;
+import Loading from 'components/Loading';
+
+const Content = ({ children, loading }) => (
+  <div className="content">{loading ? <Loading /> : children}</div>
+);
 
 export default Content;
 
 Content.propTypes = {
   children: PropTypes.oneOfType([PropTypes.arrayOf(PropTypes.node), PropTypes.node]),
+  loading: PropTypes.bool,
 };
